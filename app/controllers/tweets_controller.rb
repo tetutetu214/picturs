9482cd516e04befc35a02cf1b66119f199106aa3
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-01-11 19:39:58 +0900

    fix

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,20 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+  def new
+    @tweet = Tweet.new
+  end
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,3 @@
 class Tweet < ApplicationRecord
+  validates :name, presence: true
 end
